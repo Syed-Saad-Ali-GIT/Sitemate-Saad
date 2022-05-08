21e055b970beb9690f051eee53835b80e4dbd292
Saad <syedsaadali034@gmail.com>
2022-05-08 20:50:08 +1000

    setting up routing to different endpoints and additional file structure
    data file added

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-	console.log('SERVER RUNNING TEST')
+	if (request.url === '/api/issues' && request.method === 'GET') {
+		console.log('return all issues')
+
+	} else if (request.url.match(/\/api\/issues\/\w+/) && request.method === 'GET') {
+		const issueID = request.url.split('/')[3]
+		console.log('return issue with id ' + issueID)
+
+	} else if (request.url.match(/\/api\/issues\/\w+/) && request.method === 'DELETE') {
+		const issueID = request.url.split('/')[3]
+		console.log('delete issue with id ' + issueID)
+
+	} else if (request.url.match(/\/api\/issues\/\w+/) && request.method === 'PUT') {
+		const issueID = request.url.split('/')[3]
+		console.log('update issue with id ' + issueID)
+
+	} else if (request.url === '/api/issues' && request.method === 'POST') {
+		console.log('Create an issues')
+
+	} else {
+		response.writeHead(404, {
+			'Content-Type': 'application/json'
+		})
+		response.end(JSON.stringify({
+			message: 'Endpoint error - Not Found'
+		}))
+	}
 })
 
 
